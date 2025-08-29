d2d1816e2faa63806bd7bd843f2c94fcdb87e42a
ishardcoded <ishardcoded@gmail.com>
2025-08-29 21:31:59 +0500

    Приступили к новой программе, написали класс Document

--- OOP/Program.cs
+++ OOP/Program.cs
@@ -6,189 +6,44 @@ using System.Threading.Tasks;
 
 namespace OOP {
 
-    abstract class Figure
+    // abstract Document { id, author, abstract ShowData() }
+    abstract class Document
     {
-        protected string name;
+        private string id;
+        private string author;
 
-        public string GetName()
+        public string Id
         {
-            return name;
+            get { return id; }
+            set { id = value; }
         }
 
-        public void SetName(string name)
+        public string Author
         {
-            this.name = name;
+            get { return author; }
+            set { author = value; }
         }
 
-        public abstract double Area();
-
-        public virtual void ShowData()
+        public Document(string id, string author)
         {
-            Console.WriteLine($"Фигура: {GetName()}");
-            Console.WriteLine($"Площадь: {Area():F2}");
+            Id = id;
+            Author = author;
         }
+
+        public abstract void ShowData();
     }
 
-    class Circle : Figure
-    {
-        private double radius;
+    // TextDocument : Document { content, override ShowData() }
 
-        public double GetRadius()
-        {
-            return radius;
-        }
+    // SignedDocument : Document { signature, override ShowData() }
 
-        public void SetRadius(double radius)
-        {
-            if (radius < 0)
-            {
-                throw new ArgumentException("Радиус должен быть больше нуля");
-            }
-
-            this.radius = radius;
-        }
-
-        public Circle(double radius)
-        {
-            SetName("Круг");
-            SetRadius(radius);
-        }
-
-        public override double Area()
-        {
-            // Pi * r^2
-            return Math.PI * radius * radius;
-        }
-
-        public override void ShowData()
-        {
-            base.ShowData();
-            Console.WriteLine($"Радиус: {radius}");
-        }
-    }
-
-    class Rectangle : Figure
-    {
-        // создать свойства ширины и высоты
-        private double width;
-        private double height;
-
-        // создать методы для работы со свойствами
-        public double GetWidth()
-        {
-            return width;
-        }
-
-        public void SetWidth(double width)
-        {
-            if (width < 0)
-            {
-                throw new ArgumentException("Ширина должна быть больше нуля");
-            }
-
-            this.width = width;
-        }
-
-        public double GetHeight()
-        {
-            return height;
-        }
-
-        public void SetHeight(double height)
-        {
-            if (height < 0)
-            {
-                throw new ArgumentException("Высота должна быть больше нуля");
-            }
-
-            this.height = height;
-        }
-
-        public Rectangle(double width, double height)
-        {
-            SetName("Прямоугольник");
-            SetWidth(width);
-            SetHeight(height);
-        }
-
-        // реализовать методы Area() и ShowData()
-        public override double Area()
-        {
-            return width * height;
-        }
-
-        public override void ShowData()
-        {
-            base.ShowData();
-            Console.WriteLine($"Ширина: {width}");
-            Console.WriteLine($"Высота: {height}");
-        }
-    }
-
-    class Triangle : Figure
-    {
-        private double @base;
-        private double height;
-
-        public double GetBase()
-        {
-            return @base;
-        }
-
-        public void SetBase(double @base)
-        {
-            this.@base = @base;
-        }
-
-        public double GetHeight()
-        {
-            return height;
-        }
-
-        public void SetHeight(double height)
-        {
-            if (height < 0)
-            {
-                throw new ArgumentException("Основание должно быть больше нуля");
-            }
-            this.height = height;
-        }
-
-        public Triangle(double @base, double height)
-        {
-            SetName("Треугольник");
-            SetBase(@base);
-            SetHeight(height);
-        }
-
-        public override double Area()
-        {
-            return @base * height * 0.5;
-        }
-
-        public override void ShowData()
-        {
-            base.ShowData();
-            Console.WriteLine($"Основание: {@base}");
-            Console.WriteLine($"Высота: {height}");
-        }
-    }
     internal class Program
     {
         static void Main(string[] args)
         {
-            Figure[] figures = new Figure[]
-            {
-                new Circle(5),
-                new Rectangle(2, 5),
-                new Triangle(10, 5)
-            };
-
-            foreach(var figure in figures)
-            {
-                figure.ShowData();
-                Console.WriteLine();
-            }
+            
+            Guid uuid = Guid.NewGuid();
+            Console.WriteLine(uuid.ToString());
 
             Console.ReadKey();
         }      
