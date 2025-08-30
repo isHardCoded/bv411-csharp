21efc4ecc2f5a777eeb0da74ab55fe562a634d9f
ishardcoded <ishardcoded@gmail.com>
2025-08-30 20:50:30 +0500

    Реализовали класс TestDrive по принципу Open/Closed

--- OOP/Program.cs
+++ OOP/Program.cs
@@ -27,10 +27,45 @@ namespace OOP {
         }
     }
 
+    public class Motorcycle : IVehicle
+    {
+        public void StartEngine()
+        {
+            Console.WriteLine("Двигатель мотоцикла запущен");
+        }
+
+        public void StopEngine()
+        {
+            Console.WriteLine("Двигатель мотоцикла остановлен");
+        }
+    }
+
+    public class Plane : IVehicle
+    {
+        public void StartEngine()
+        {
+            Console.WriteLine("Двигатель самолета запущен");
+        }
+
+        public void StopEngine()
+        {
+            Console.WriteLine("Двигатель самолета остановлен");
+        }
+    }
+
     // в каждом классе написать реализацию методов StartEngine() и StopEngine()
 
     // создать в программе класс TestDrive, который будет принимать объект типа IVehicle
     // и последовательно вызывать для него методы StartEngine() и StopEngine()
+
+    public class TestDrive
+    {
+        public void StartTest(IVehicle vehicle)
+        {
+            vehicle.StartEngine();
+            vehicle.StopEngine();
+        }
+    }
 
     // создать объекты Car и Motorcycle
 
@@ -38,7 +73,19 @@ namespace OOP {
     {
         static void Main(string[] args)
         {
-            
+            IVehicle[] vehicles = new IVehicle[]
+            {
+                new Car(),
+                new Motorcycle(),
+                new Plane(),
+            };
+
+            TestDrive testDrive = new TestDrive();
+
+            foreach (var vehicle in vehicles)
+            {
+                testDrive.StartTest(vehicle);
+            }
 
             Console.ReadKey();
         }      
