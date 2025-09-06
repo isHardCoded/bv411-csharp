139faeb72fe94eebeb2bbd349514a7be372e1577
ishardcoded <ishardcoded@gmail.com>
2025-09-06 19:55:58 +0500

    Создали класс TextFile, реализовали в нем метод Read для чтения текстовых файлов

--- OOP/Program.cs
+++ OOP/Program.cs
@@ -7,18 +7,52 @@ using System.IO;
 
 namespace OOP {
 
+    public class TextFile
+    {
+        public string filePath { get; private set; }
+
+        public TextFile(string filePath)
+        {
+            this.filePath = filePath;
+        }
+
+        public string Read()
+        {
+            try
+            {
+                if (!File.Exists(filePath))
+                {
+                    throw new FileNotFoundException("Файл не найден", filePath);
+                }
+
+                using (StreamReader reader = new StreamReader(filePath))
+                {
+                    return reader.ReadToEnd();
+                }
+            }
+            catch (FileNotFoundException ex)
+            {
+                Console.WriteLine("Ошибка: " + ex.Message);
+                return null;
+                
+            }
+        }
+    }
     internal class Program
     {
         static void Main(string[] args)
         {
-            string[] lines = { "Line 1", "Line 2", "Line 3" };
-            using (StreamWriter writer = new StreamWriter("file.txt", append: true))
-            {
-                foreach (string line in lines)
-                {
-                    writer.WriteLine(line);
-                }
-            }
+            Console.Write("Введите путь к файлу: ");
+            string path = Console.ReadLine();
+
+            Console.Write("Введите текст, который хотите записать в файл: ");
+            string textToWrite = Console.ReadLine();
+
+            TextFile file = new TextFile(path);
+
+            string fileContext = file.Read();
+            Console.WriteLine("Содержимое файла:");
+            Console.Write(fileContext);
 
             Console.ReadKey();
         }      
