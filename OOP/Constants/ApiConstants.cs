d755fd84e0abdd10f4fc7fd311cefdb5a276fb2a
ishardcoded <ishardcoded@gmail.com>
2025-09-23 20:53:41 +0500

    Определили модули приложения, вынесли url для запросов в BaseUrl в файл ApiConstants.cs

---
+++ OOP/Constants/ApiConstants.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP.Constants
+{
+    internal class ApiConstants
+    {
+        public const string BaseUrl = "https://jsonplaceholder.typicode.com";
+    }
+}
